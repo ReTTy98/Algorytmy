b6af89d7cfae809b6aad8cca120ecca44b7e6293
ReTTy98 <43709151+ReTTy98@users.noreply.github.com>
2020-03-06 14:00:53 +0100

    jebane floaty

--- lista1/zad4.py
+++ lista1/zad4.py
@@ -2,29 +2,58 @@ import numpy as np
 import string
 import random
 
+
 def create_bill():
-    pass
+    bill = np.empty(random.randint(6, 8), )
 
 
 def available_products():
+    products_array = np.zeros((random.randint(4, 9), 3), dtype=float)
+    row_quantity = np.shape(products_array)[0]
+    print(products_array)
+    print(row_quantity)
+    products_array[:, 0] = product_code_generate(row_quantity)
+    print(products_array)
+    products_array[:, 1] = prices_generate(row_quantity)
+    products_array[:,2] = product_type_generate(row_quantity)
+    print(products_array)
 
 
+def testing_bill():
+    pass
 
-def product_code_generate(how_many, size=6, chars=string.ascii_uppercase + string.digits):
+
+def product_code_generate(how_many, size=4, chars=string.digits):
     products_codes = []
     for i in range(how_many):
-        products_codes.append(''.join(random.choice(chars) for i in range(size)))
+        x = ''.join(random.choice(chars) for i in range(size))
+        x = int(x)
+        products_codes.append(x)
+        # products_codes.append(''.join(random.choice(chars) for i in range(size)))
     return products_codes
 
 
+def prices_generate(how_many):
+    prices = []
+    for i in range(how_many):
+        x = round(random.uniform(1, 30),2)
+        prices.append(x)
+    print(f"GOTOWKA:{prices}")
+    return prices
+
+def product_type_generate(how_many):
+    product_type = []
+    for i in range(how_many):
+        x = random.randint(0,1)
+        product_type.append(x)
+    return product_type
+
 if __name__ == "__main__":
-    print(product_code_generate(4))
-
-
-
+    available_products()
 
 """
-[numer produktu , nazwa produktu, Czy w kg? , cena za szt/kg , ilosc]
+Paragon = [Nr.Klenta , numer towary , szt/kg  ]
+Dostepne produkty = [numer towaru, cene-szt/kg , szt/wg?]
 
 
 
